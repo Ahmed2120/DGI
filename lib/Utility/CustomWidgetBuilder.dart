94f703e4d727a4d538f66c79603d807000097f62
Mai <mai.samir896@gmail.com>
2022-03-23 21:37:59 +0200

    add scan bar code

--- lib/Utility/CustomWidgetBuilder.dart
+++ lib/Utility/CustomWidgetBuilder.dart
@@ -2,5 +2,36 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CustomWidgetBuilder{
+  static buildTextFormField(Size dSize,String title,String text){
+    return Row(
+      children: [
+        buildText(title, dSize),
+        const Spacer(),
+        Container(
+          width: dSize.width * 0.4,
+          decoration: const BoxDecoration(
+            border: Border(
+                bottom: BorderSide(
+                    color: Color(0xFF00B0BD), width: 2)),
+          ),
+          child: TextFormField(
+            controller:TextEditingController(text: text) ,
+            enabled: false,
+            decoration: const InputDecoration(
+              constraints: BoxConstraints(maxHeight: 20),
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+  static Text buildText(String title, dSize) {
+    return Text(
+      title,
+      style:
+      TextStyle(fontSize: dSize.width * 0.04, color: Color(0xFF0F6671), fontWeight: FontWeight.bold),
+    );
+  }
 
 }
--- lib/screens/item_capture_screen.dart
+++ lib/screens/item_capture_screen.dart
@@ -4,6 +4,7 @@ import 'package:dgi/Services/CityService.dart';
 import 'package:dgi/Services/CountryService.dart';
 import 'package:dgi/Services/DepartmentService.dart';
 import 'package:dgi/Services/FloorService.dart';
+import 'package:dgi/Utility/CustomWidgetBuilder.dart';
 import 'package:dgi/Utility/DropDownMenu.dart';
 import 'package:dgi/model/area.dart';
 import 'package:dgi/model/assetLocation.dart';
@@ -152,19 +153,19 @@ class _ItemCaptureState extends State<ItemCapture> {
                             SizedBox(height: dSize.height * 0.01,),
                             DropDownMenu(title: "CITY", values: cities.map((e) => e.name).toList()),
                             SizedBox(height: dSize.height * 0.01,),
-                            buildTextFormField(dSize,'AREA',areas.isNotEmpty?areas[0].name:'dump'),
+                            CustomWidgetBuilder.buildTextFormField(dSize,'AREA',areas.isNotEmpty?areas[0].name:'dump'),
                             SizedBox(height: dSize.height * 0.01,),
-                            buildTextFormField(dSize,'DEPARTMENT',departments.isNotEmpty?departments[0].name:"dump"),
+                            CustomWidgetBuilder.buildTextFormField(dSize,'DEPARTMENT',departments.isNotEmpty?departments[0].name:"dump"),
                             SizedBox(height: dSize.height * 0.01,),
-                            buildTextFormField(dSize,'BUSINESS UNIT',assetLocation!.businessUnit),
+                            CustomWidgetBuilder.buildTextFormField(dSize,'BUSINESS UNIT',assetLocation!.businessUnit),
                             SizedBox(height: dSize.height * 0.01,),
-                            buildTextFormField(dSize,'NAME',assetLocation!.name),
+                            CustomWidgetBuilder.buildTextFormField(dSize,'NAME',assetLocation!.name),
                             SizedBox(height: dSize.height * 0.01,),
-                            buildTextFormField(dSize,'BLDG NAME',assetLocation!.buildingName),
+                            CustomWidgetBuilder.buildTextFormField(dSize,'BLDG NAME',assetLocation!.buildingName),
                             SizedBox(height: dSize.height * 0.01,),
-                            buildTextFormField(dSize,'BLDG ADDRESS',assetLocation!.buildingAddress),
+                            CustomWidgetBuilder.buildTextFormField(dSize,'BLDG ADDRESS',assetLocation!.buildingAddress),
                             SizedBox(height: dSize.height * 0.01,),
-                            buildTextFormField(dSize,'BUILDING NO',assetLocation!.buildingNo.toString()),
+                            CustomWidgetBuilder.buildTextFormField(dSize,'BUILDING NO',assetLocation!.buildingNo.toString()),
                             SizedBox(height: dSize.height * 0.01,),
                             DropDownMenu(title: 'FLOOR NO', values: floors.map((e) => e.name).toList()),
                           ],
@@ -240,25 +241,7 @@ class _ItemCaptureState extends State<ItemCapture> {
         ));
   }
 
-  Text buildText(String title, dSize) {
-    return Text(
-      title,
-      style:
-      TextStyle(fontSize: dSize.width * 0.04, color: Color(0xFF0F6671), fontWeight: FontWeight.bold),
-    );
-  }
-
   initData() async{
-/*
-    await cityService.insert(City(name: 'Cairo'));
-    await countryService.insert(Country(name: 'Egypt'));
-    await areaService.insert(Area(name: 'helwan'));
-    await floorService.insert(Floor(name: '8'));
-    await departmentService.insert(Department(name: 'dep'));
-    await assetLocationService.insert(AssetLocation(name: "location",areaId: 1,buildingAddress: "test building Address",
-    buildingName: "building Name",buildingNo: '10',businessUnit: 'businessUnit',departmentId: 10,floorId: 10,id: 10));
-*/
-
     countries = await countryService.retrieve();
     cities = await cityService.retrieve();
     floors = await floorService.retrieve();
@@ -274,30 +257,5 @@ class _ItemCaptureState extends State<ItemCapture> {
     setState(() {
 
     });
-  }
-
-  buildTextFormField(Size dSize,String title,String text){
-    return Row(
-      children: [
-        buildText(title, dSize),
-        const Spacer(),
-        Container(
-          width: dSize.width * 0.4,
-          decoration: const BoxDecoration(
-            border: Border(
-                bottom: BorderSide(
-                    color: Color(0xFF00B0BD), width: 2)),
-          ),
-          child: TextFormField(
-            controller:TextEditingController(text: text) ,
-            enabled: false,
-            decoration: const InputDecoration(
-              constraints: BoxConstraints(maxHeight: 20),
-              border: InputBorder.none,
-            ),
-          ),
-        ),
-      ],
-    );
   }
 }
